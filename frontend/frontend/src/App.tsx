5fe131ff94ae708a545b4a4362de43984dac81eb
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2025-03-17 18:52:15 +0100

    Interactivity for input element implemented

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+import {useState, useEffect} from 'react';
 import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 import PlaceInput from './components/PlaceInput';
@@ -7,18 +6,28 @@ import WaitComponent from './components/WaitComponent';
 import TripList from './components/TripList';
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
+    const [targetDestination, setTargetDestination] = useState("");
+    const [duration, setDuration] = useState("");
+    const [started, setStarted] = useState(false);
+    const [ready, setReady] = useState(false);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+    useEffect(() => {
+        if (started) {
+            console.log("call api");
+            const wait = async () => {
+                await new Promise(resolve => setTimeout(resolve, 5000));
+                console.log("waiting...");
+                setStarted(false);
+            };
+
+            wait();
+        }
+    }, [started]);
+
 
     return (
         <div id="App">
-            <PlaceInput />
+            <PlaceInput destination={targetDestination} setDestination={setTargetDestination} duration={duration} setDuration={setDuration} setStarted={setStarted}/>
             <WaitComponent />
             <TripList days={[{"dayName": "day 1", "dayActivities": [{"activityName": "swimming", "activityDescription": "Swimming with friends"}, {"activityName": "running", "activityDescription": "Running with friends"}]}, {"dayName": "day 2", "dayActivities": [{"activityName": "swimming", "activityDescription": "Swimming with friends"}]}]}/>
         </div>
--- frontend/frontend/src/components/PlaceInput.tsx
+++ frontend/frontend/src/components/PlaceInput.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import "../assets/style/PlaceInput.css";
 
+interface InputProps {
+    destination: string;
+    setDestination: (destination: string) => void;
+    duration: string;
+    setDuration: (duration: string) => void;
+    setStarted: (started: boolean) => void;
+}
 
-export default function PlaceInput() {
+
+export default function PlaceInput({destination, setDestination, duration, setDuration, setStarted}: InputProps) {
     return <div className="input-container">
         <div className="input-field" id="rounded-input">
             <h4>Where to?</h4>
-            <input id="destination" placeholder="Country, city"/>
+            <input id="destination" placeholder="Country, city" value={destination} onChange={(e) => setDestination(e.target.value)}/>
         </div>
         <div className="input-field">
             <h4>How long?</h4>
-            <input id="duration" placeholder="days, weeks"/>
+            <input id="duration" placeholder="days, weeks" value={duration} onChange={(e) => setDuration(e.target.value)}/>
         </div>
-        <button id="confirm">Confirm</button>
+        <button id="confirm" onClick={() => setStarted(true)}>Confirm</button>
     </div>
 }
