1027464e4f3d31b6e2c55e80d2edbf41858dcae3
Jakub Milasz <jakubmilasz@gmail.com>
2025-03-19 10:08:25 +0100

    Connection with Gemini

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+import google.generativeai as genai
+
+# załadowanie klucza i połączenie z gemini api
+_ = load_dotenv(find_dotenv())
+genai.configure(api_key=os.environ.get("GOOGLE_AI_API_KEY"))
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+prompt_template = """Zaplanuj wycieczkę po {country} na {date}.""" # przykład szablonu
+
+# funkcja do ła
+def generate_response(prompt, country, date):
+  filled_prompt = prompt.format(country=country, date=date, prompt=prompt)
+  response = model.generate_content(filled_prompt)
+  return response.text
+
+print(generate_response("Włochy", "7 dni", prompt_template)) # użycie szablonu
