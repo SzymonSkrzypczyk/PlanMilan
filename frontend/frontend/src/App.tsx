339fcc76a9739698405909a13c627a2651f9b4e3
JuliaK@DESKTOP-5DEEQPA <julia.kot1237@gmail.com>
2025-04-03 19:19:40 +0200

    Changing style

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -4,37 +4,38 @@ import {CallLLM} from "../wailsjs/go/main/App";
 import PlaceInput from './components/PlaceInput';
 import WaitComponent from './components/WaitComponent';
 import TripList from './components/TripList';
- 
+
+
+import logo from "./assets/images/tlogit.png";
+
+<img src={logo} alt="Logo" className="logo" />
+
 function App() {
     const [targetDestination, setTargetDestination] = useState("");
     const [duration, setDuration] = useState("");
     const [started, setStarted] = useState(false);
     const [ready, setReady] = useState(false);
-    const [isFinishedProcessing , setFinishedProcesssing] = useState(false);
-    const [tripData, setTripData] = useState<{ 
-        dzień: string; 
-        aktywnosci: { 
-            nazwa_aktywnosci: string; 
-            opis_aktywnosci: string; 
+    const [tripData, setTripData] = useState<{
+        dzień: string;
+        aktywnosci: {
+            nazwa_aktywnosci: string;
+            opis_aktywnosci: string;
             kiedy?: string;
-        }[]; 
+        }[];
     }[]>([]);
 
     useEffect(() => {
         if (started) {
             CallLLM(targetDestination, duration ).then((response) => {
                 let data = JSON.parse(response);
-                // in case of an error
-                setFinishedProcesssing(false);
                 setReady(true);
                 setStarted(false);
                 setTripData(data);
-                setFinishedProcesssing(true);
             }
             ).catch((error) => {
                 console.log(error);
                 setReady(false);
-                setStarted(false); 
+                setStarted(false);
             }
             ).finally(() => {
                 setStarted(false);
@@ -46,14 +47,40 @@ function App() {
 
     return (
         <div id="App">
-            <h1>Planer podróży</h1>
+
+            <div className="header">
+
+
+
+                    <div className="logos">
+                        <img src={logo} alt="Logo" className="logo" />
+                        <img src={logo} alt="Logo" className="logo" />
+                    </div>
+
+                    <h1 className="title">
+                        Pl<span className="highlight">A</span>nM<span className="highlight">I</span>lan
+                    </h1>
+
+                    <div className="logos">
+                        <img src={logo} alt="Logo" className="logo" />
+                        <img src={logo} alt="Logo" className="logo" />
+                    </div>
+
+            </div>
+
+
             <PlaceInput destination={targetDestination} setDestination={setTargetDestination} duration={duration} setDuration={setDuration} setStarted={setStarted} setReady={setReady}/>
             {started && !ready ? (
                 <WaitComponent loading={started} />
             ) : (
-                <TripList days={tripData || []} finished={isFinishedProcessing }/>
+                <TripList days={tripData || []} />
             )}
             {/*<TripList days={[{"dayName": "day 1", "dayActivities": [{"activityName": "swimming", "activityDescription": "Swimming with friends"}, {"activityName": "running", "activityDescription": "Running with friends"}]}, {"dayName": "day 2", "dayActivities": [{"activityName": "swimming", "activityDescription": "Swimming with friends"}]}]}/>*/}
+            {/* Stopka */}
+                <footer className="footer">
+                    <p>&copy; PlanMilan generuje rekomendacje podróżnicze na podstawie analizy AI. Zalecamy jednak przed podjęciem decyzji o podróży skonsultować się z lokalnym ekspertem lub przewodnikiem, aby uzyskać najbardziej aktualne i dostosowane do indywidualnych potrzeb informacje.</p>
+                </footer>
+
         </div>
     )
 }
--- frontend/frontend/src/components/TripList.tsx
+++ frontend/frontend/src/components/TripList.tsx
@@ -4,17 +4,24 @@ import "../assets/style/TripList.css";
 
 interface TripListProps{
     days: DayProps[];
-    finished: boolean;
 }
 
 
-export default function TripList({days, finished}: TripListProps) {
+export default function TripList({days}: TripListProps) {
     return <div className="trip-layout">
-        {finished && <h2>Twój plan:</h2>}
-        <div id="trips-list">
-            {days.map((day, index) => {
-                return <TripDayComponent key={index} dzień={day.dzień} aktywnosci={day.aktywnosci}/>
-            })} 
+
+            <h2>Twój plan:</h2>
+            <div id="trips-list">
+                {days.map((day, index) => {
+                    return (
+
+                            <TripDayComponent
+                                dzień={day.dzień}
+                                aktywnosci={day.aktywnosci}
+              />
+
+          );
+                })}
+            </div>
         </div>
-    </div>
 }
